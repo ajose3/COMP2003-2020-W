bdb1de6251322a7b874bb3ba247075676e4e7c8d
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2020-12-14 01:18:55 +0000

    Customer Interface File Layout

---
+++ CustomerInterface/CustomerInterface/CustomerInterface/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CustomerInterface.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CustomerInterface.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}

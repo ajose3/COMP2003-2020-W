cbf14513fb336822255f1ca2b4ad6790bf4b3f40
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2020-12-18 12:15:00 +0000

    ran app for first time in main

--- MobileApp/MobileApp/MobileApp.Android/obj/Debug/90/android/environment.x86.s
+++ MobileApp/MobileApp/MobileApp.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"c27ddd14-56d5-4dcd-be13-76150bcbbeb9"
+	.asciz	"ae631550-7c15-475a-9cc5-ba5bd07d3769"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object

4d38abcbab3465f8fe8be0ece30cb8802e89d735
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2021-02-15 20:46:30 +0000

    fixed carousel click event

--- MobileApp/MobileApp/MobileApp.Android/obj/Debug/100/android/environment.armeabi-v7a.s
+++ MobileApp/MobileApp/MobileApp.Android/obj/Debug/100/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"77cbea44-df0a-42c1-9102-2a68982736e3"
+	.asciz	"89f24891-3bde-4536-a2d0-1549c016c9ab"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object

75de557281318ac086cb0114e7020fd8f05724e0
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2021-03-02 13:16:39 +0000

    Added login functionality

--- Admin Interface/AdminInterface/AdminInterface/Controllers/LoginController.cs
+++ Admin Interface/AdminInterface/AdminInterface/Controllers/LoginController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using AdminInterface.Models;
+using AdminInterface.Services;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,6 +17,32 @@ namespace AdminInterface.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(string email, string password)
+        {
+            WebDataService dataService = new WebDataService();
+            await dataService.GetValidateAdmin(email, password);
+            //contact api (admin login)
+            if (Token.value == null || Token.value == "1")
+            {
+                //display alert saying wrong password
+                //return Redirect("~/Home/Privacy");
+                //return "<script>alert('Data inserted successfully')</script>"
+                //return Content("<script language='javascript' type='text/javascript'>alert('Hello world!');</script>");
+                return Redirect("~/Login");
+
+            }
+
+            //if password correct
+            else
+            {
+                return Redirect("~/Home");
+            }
+            
+
+
+
         }
     }
 }
---
+++ Admin Interface/AdminInterface/AdminInterface/Services/WebDataService.cs
@@ -0,0 +1,32 @@
+﻿using AdminInterface.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AdminInterface.Services
+{
+    public class WebDataService : IDataService
+    {
+        HttpClient httpClient;
+
+        HttpClient Client => httpClient ?? (httpClient = new HttpClient());
+        public async Task GetValidateAdmin(string email, string password)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"http://web.socem.plymouth.ac.uk/comp2003/COMP2003_W/Admin/Validate?email={email}&password={password}"),
+                //Content = new StringContent(json, Encoding.UTF8, "application/json"),
+            };
+
+            var response = await Client.SendAsync(request).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+            string returnedJson = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            returnedJson = returnedJson.Replace("/", "").Replace("\"", "");
+            Token.value = returnedJson;
+        }
+    }
+}

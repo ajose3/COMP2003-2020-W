3cf93c83dbd6347f5a93644a405920429cac1996
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2021-03-07 18:05:40 +0000

    Visual Improvements

--- MobileApp/MobileApp/MobileApp.Android/obj/Debug/100/android/environment.arm64-v8a.s
+++ MobileApp/MobileApp/MobileApp.Android/obj/Debug/100/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"50779e4b-2783-455b-9fce-126216960543"
+	.asciz	"4e70c4d1-1e59-498d-bdd6-2d28f8923537"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- MobileApp/MobileApp/MobileApp/Views/HomePage.xaml.cs
+++ MobileApp/MobileApp/MobileApp/Views/HomePage.xaml.cs
@@ -31,7 +31,10 @@ namespace MobileApp.Views
             //Logic to handle updated search bar text
         } */
 
-        //Search handler
+        
+
+
+        //collection view select
         async void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             // This works because route names are unique in this application.

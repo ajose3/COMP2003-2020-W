cefb4f0c47b44abaa7942f278c6e4497a32e8a6a
alistair <alistair.james@students.plymouth.ac.uk>
2021-03-17 11:04:10 +0000

    Search results product selection

--- MobileApp/MobileApp/MobileApp/Views/SearchPage.xaml.cs
+++ MobileApp/MobileApp/MobileApp/Views/SearchPage.xaml.cs
@@ -24,7 +24,10 @@ namespace MobileApp.Views
         //collection view select
         async void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
+            // This works because route names are unique in this application.
+            int productId = (e.CurrentSelection.FirstOrDefault() as Product).Id;
+            // query product id as all products will need a unqiue id
+            await Shell.Current.GoToAsync($"productdetails?id={productId}");
         }
     }
 }

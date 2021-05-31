3a2f7d7a7cd5e3a8371cfb5c0ecd0b0b92b7d25b
alistair <alistair.james@students.plymouth.ac.uk>
2021-05-31 21:56:23 +0100

    Updated Admin Dashboard

--- Admin Interface/AdminInterface/AdminInterface/Controllers/HomeController.cs
+++ Admin Interface/AdminInterface/AdminInterface/Controllers/HomeController.cs
@@ -3,25 +3,47 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace AdminInterface.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly COMP2003_WContext _context;
+
+        public HomeController(COMP2003_WContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            // get orders
+            List<Orders> orders = await _context.Orders.ToListAsync();
+            // sort orders so that first is most recent
+            orders = orders.OrderByDescending(o => o.TimeOrdered.Date).ThenByDescending(o => o.TimeOrdered.TimeOfDay).ToList();
+            // make sorted orders available to view
+            ViewData["Orders"] = orders;
+
+            return View(await _context.Products.ToListAsync());
+        }
+
+
+        /*private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
+        }*/
 
-        public IActionResult Index()
+        /*public IActionResult Index()
         {
             return View();
-        }
+        }*/
 
         public IActionResult Privacy()
         {
